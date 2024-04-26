bf2780047415ef17aba76c29a6326e9f4abb340f
Fabrice THIROUX <fabrice.thiroux@net-c.com>
2024-04-26 15:18:35 +0200

    data_source: Implement data source client functionality

--- internal/provider/coffees_data_source.go
+++ internal/provider/coffees_data_source.go
@@ -2,14 +2,17 @@ package provider
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/hashicorp-demoapp/hashicups-client-go"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
 var (
-	_ datasource.DataSource = &coffeesDataSource{}
+	_ datasource.DataSource              = &coffeesDataSource{}
+	_ datasource.DataSourceWithConfigure = &coffeesDataSource{}
 )
 
 // NewCoffeesDataSource is a helper function to simplify the provider implementation.
@@ -18,7 +21,9 @@ func NewCoffeesDataSource() datasource.DataSource {
 }
 
 // coffeesDataSource is the data source implementation.
-type coffeesDataSource struct{}
+type coffeesDataSource struct {
+	client *hashicups.Client
+}
 
 // Metadata returns the data source type name.
 func (d *coffeesDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
@@ -32,4 +37,23 @@ func (d *coffeesDataSource) Schema(_ context.Context, _ datasource.SchemaRequest
 
 // Read refreshes the Terraform state with the latest data.
 func (d *coffeesDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+}
+
+// Configure adds the provider configured client to the data source.
+func (d *coffeesDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*hashicups.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *hashicups.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
 }
